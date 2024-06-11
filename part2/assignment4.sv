277bdcbaaf128eeeb250ae2fdcb5f7f1fceb648e
Bruce Xi <91307696+brucexi999@users.noreply.github.com>
2024-06-11 12:29:10 -0700

    Update

--- part2/assignment4.sv
+++ part2/assignment4.sv
@@ -84,48 +84,190 @@ output reg sclk,cs,mosi
   
 endmodule
 
+interface SPI_IF;
+    logic clk, sclk, newd, rst, cs, mosi;
+    logic [11:0] din;
 
+endinterface
 
+class Transaction;
+    bit newd;
+    randc bit [11:0] din;
+    bit [11:0] dout;
 
+    function Transaction copy();
+        copy = new();
+        copy.newd = this.newd;
+        copy.din = this.din;
+        copy.dout = this.dout;
+    endfunction
+endclass
 
+class Generator;
+    Transaction transaction;
+    mailbox #(Transaction) mail_box_gen_dri, mail_box_gen_sco;
+    int count = 10;
+    event send_next_stimulus;
 
-module tb(  );
- 
-reg clk = 0, rst = 0, newd = 0;
-reg [11:0] din = 0;
-wire sclk, cs, mosi;
-  reg [11:0] mosi_out;
- 
-always #10 clk = ~clk;
- 
-spi_master dut (clk, newd,rst, din, sclk, cs, mosi);
- 
-initial 
-begin
-rst = 1;
-repeat(5) @(posedge clk);
-rst = 0;
- 
-newd = 1;
-din = $urandom;
-  $display("%0d", din); 
-  for(int i = 0; i <= 11; i++)
-    begin
-    @(negedge dut.sclk);
-    mosi_out = {mosi, mosi_out[11:1]};
-    $display("%0d", mosi_out);  
+    function new(mailbox #(Transaction) mail_box_gen_dri, mail_box_gen_sco);
+        this.mail_box_gen_dri = mail_box_gen_dri;
+        this.mail_box_gen_sco = mail_box_gen_sco;
+        transaction = new();
+    endfunction
+
+    task run();
+        repeat(count) begin
+            transaction.randomize();
+            mail_box_gen_dri.put(transaction.copy());
+            mail_box_gen_sco.put(transaction.copy());
+            @(send_next_stimulus); 
+        end
+    endtask
+
+endclass
+
+class Driver;
+    Transaction transaction;
+    mailbox #(Transaction) mail_box;
+    virtual SPI_IF spi_if;
+
+    function new(mailbox #(Transaction) mail_box);
+        this.mail_box = mail_box;
+    endfunction
+
+    task reset();
+        spi_if.rst <= 1;
+        spi_if.newd <= 0;
+        spi_if.din <= 0;
+        repeat(10) @(posedge spi_if.clk);
+        spi_if.rst <= 0;
+        repeat(10) @(posedge spi_if.clk);
+        $display("[Driver] : Reset done");
+        $display("-----------------------------------------");
+    endtask
+
+    task run();
+        forever begin
+            mail_box.get(transaction);
+            @(posedge spi_if.sclk);
+            spi_if.newd <= 1;
+            spi_if.din <= transaction.din;
+            @(posedge spi_if.sclk);
+            spi_if.newd <= 0;
+            wait(spi_if.sclk == 1);
+            $display("[Driver] : Data sent to DUT : %0d",transaction.din);
+        end
+    endtask
+endclass
+
+class Monitor;
+    Transaction transaction;
+    mailbox #(Transaction) mail_box;
+    virtual SPI_IF spi_if;
+
+    function new(mailbox #(Transaction) mail_box);
+        this.mail_box = mail_box;
+        transaction = new();
+    endfunction
+
+    task run();
+        forever begin
+            @(negedge spi_if.sclk);
+            wait(spi_if.cs == 0);
+            //@(negedge spi_if.sclk);
+            for (int i = 0; i < 12; i ++) begin
+                @(negedge spi_if.sclk);
+                transaction.dout[i] = spi_if.mosi;
+            end
+            wait(spi_if.cs == 1);
+            $display("[Monitor] : Data sent to scoreboard : %0d", transaction.dout);
+            mail_box.put(transaction);
+        end
+    endtask
+endclass
+
+class Scoreboard;
+    Transaction transaction_gen_sco, transaction_mon_sco;
+    mailbox #(Transaction) mail_box_gen_sco;
+    mailbox #(Transaction) mail_box_mon_sco;
+    event send_next_stimulus;
+
+    function new(mailbox #(Transaction) mail_box_gen_sco, mailbox #(Transaction) mail_box_mon_sco);
+        this.mail_box_gen_sco = mail_box_gen_sco;
+        this.mail_box_mon_sco = mail_box_mon_sco;
+    endfunction
+
+    task run();
+        forever begin
+            mail_box_mon_sco.get(transaction_mon_sco);
+            mail_box_gen_sco.get(transaction_gen_sco);
+            $display("[Scoreboard] : DRV : %0d MON : %0d", transaction_mon_sco.dout, transaction_gen_sco.din);
+            assert (transaction_mon_sco.dout == transaction_gen_sco.din) else $error("[Scoreboard] : DATA MISMATCHED!");
+            $display("-----------------------------------------");
+            -> send_next_stimulus;
+        end
+    endtask
+
+endclass
+
+class Environment;
+    Generator generator;
+    Driver driver;
+    Monitor monitor;
+    Scoreboard scoreboard;
+    mailbox #(Transaction) mbx_gen_dri, mbx_gen_sco, mbx_mon_sco;
+    virtual SPI_IF spi_if;
+    event send_next_stimulus;
+
+    function new(virtual SPI_IF spi_if);
+        mbx_gen_dri = new();
+        mbx_gen_sco = new();
+        mbx_mon_sco = new();
+        generator = new(mbx_gen_dri, mbx_gen_sco);
+        driver = new(mbx_gen_dri);
+        monitor = new(mbx_mon_sco);
+        scoreboard = new(mbx_gen_sco, mbx_mon_sco);
+        driver.spi_if = spi_if;
+        monitor.spi_if = spi_if;
+        generator.send_next_stimulus = send_next_stimulus;
+        scoreboard.send_next_stimulus = send_next_stimulus;
+    endfunction
+
+    task run();
+        driver.reset();
+        fork
+            generator.run();
+            driver.run();
+            monitor.run();
+            scoreboard.run();
+        join_any
+        $finish();
+    endtask
+endclass
+
+module tb;
+    SPI_IF spi_if();
+
+    initial begin
+        spi_if.clk <= 0;
     end
-  
-  
-end
- 
- 
- 
-  initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars;
-    #2500;
-    $stop;
-  end
- 
+
+    always #5 spi_if.clk <= ~spi_if.clk;
+
+    spi_master dut (
+        spi_if.clk,
+        spi_if.newd,
+        spi_if.rst,
+        spi_if.din,
+        spi_if.sclk,
+        spi_if.cs,
+        spi_if.mosi
+    );
+
+    Environment env;
+
+    initial begin
+        env = new(spi_if);
+        env.run();
+    end
 endmodule
